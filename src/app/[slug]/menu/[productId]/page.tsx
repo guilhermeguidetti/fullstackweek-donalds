f9d1c0016a54ec2d8a6687ba0061dba9887d336b
Guilherme Guidetti <53499339+guilhermeguidetti@users.noreply.github.com>
2025-02-27 00:06:34 -0300

    feat: add product details component and currency formatting helper

--- src/app/[slug]/menu/[productId]/page.tsx
+++ src/app/[slug]/menu/[productId]/page.tsx
@@ -1,7 +1,9 @@
+import { notFound } from "next/navigation";
 import React from "react";
 
 import { db } from "@/lib/prisma";
 
+import ProductDetails from "./components/product-details";
 import ProductHeader from "./components/product-header";
 
 interface ProductPageProps {
@@ -12,15 +14,28 @@ export default async function ProductPage({ params }: ProductPageProps) {
   const { slug, productId } = await params;
   const product = await db.product.findUnique({
     where: { id: productId },
+    include: {
+      restaurant: {
+        select: {
+          name: true,
+          avatarImageUrl: true,
+          slug: true,
+        },
+      },
+    },
   });
   if (!product) {
     return <div>Produto não encontrado</div>;
   }
 
+  if (product.restaurant.slug.toUpperCase() !== slug.toUpperCase()) {
+    return notFound();
+  }
+
   return (
-    <>
+    <div className="flex h-full flex-col">
       <ProductHeader product={product} />
-      {slug}
-    </>
+      <ProductDetails product={product} />
+    </div>
   );
 }
